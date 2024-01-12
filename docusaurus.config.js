9a64c2cdec5f3ae7a4be1cc44cbb405472ade9c8
Sean Geelan <seanpatrick.geelan@gmail.com>
2024-01-12 16:44:51 +0100

    Navvis template

---
+++ docusaurus.config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  title: "NavVis IVION Documentation",
+  tagline: "NavVis Webhelp",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.svg",
+  organizationName: "NavVis",
+  projectName: "navvis-docusaurus-template",
+  themeConfig: {
+    navbar: {
+      title: "NavVis IVION",
+      logo: {
+        alt: "NavVis VLX Documentation",
+        src: "img/navvis-logo-new.svg",
+      },
+     
+    },
+    docs: {
+      sidebar: {
+        hideable: true
+      }
+    },
+    footer: {
+      style: "dark",
+      copyright: `Copyright © ${new Date().getFullYear()} NavVis GmbH.`,
+    },
+    colorMode: {
+      respectPrefersColorScheme: true
+    }
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        blog: false,
+        pages: false,
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          path: './docs',
+          routeBasePath: '/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      require.resolve('@cmfcmf/docusaurus-search-local'),
+      {
+        indexBlog: false
+      }
+    ]
+  ]
+};
